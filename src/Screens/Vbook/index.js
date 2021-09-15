98cc621f12e93a610e15ab99db9f718a91de44e6
Hp EliteBook 840 G3 <snbz687@gmail.com>
2021-09-16 02:30:55 +0500

    Home Screen: Vbook Flow

--- src/Screens/Vbook/index.js
+++ src/Screens/Vbook/index.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
-const VbookScreen = () => {
-    return (
-        <View>
-            <Text>VbookScreen</Text>
-        </View>
+
+const SplashScreen = () => {
+    return(
+        <View style={{backgroundColor:'#563AD7', flex: 1, flexDirection:'column', padding: 20}}>
+      <View style={{ flex: 2}}>
+      </View>
+      <View style={{ flex: 3}}>
+          <Image style={{alignSelf:'center', position:'absolute', bottom:0}}
+           source={require("../../Assets/Vbook/Vbook_logo.png")}  /> 
+              </View>
+      <View style={{ flex: 4,alignItems:'center'}}>
+      </View>
+    </View>
+
     )
 }
 
-export default VbookScreen
-
-const styles = StyleSheet.create({})
+export default SplashScreen;
